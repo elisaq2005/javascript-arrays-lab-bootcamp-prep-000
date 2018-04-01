969e4e3e3cd413879f44bb48fd9a7e1f2276e8a0
elisaquiney@gmail.com <elisaquiney@gmail.com>
2018-04-01 15:15:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 
 function destructivelyAppendKitten(name) {
-  kittens = upshift.destructivelyAppendKitten(name);
-  return kittens;
+  push.destructivelyAppendKitten(name);
+  return kitten;
 }
